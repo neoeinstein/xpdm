660ac913086d761a1551d1d9029c3e68f736e9c1
Marcus Griep <marcus@griep.us>
2009-06-18 23:29:01 -0400

    Mark Xpdm.Math as CLS-Compliant

--- Xpdm.Math/AssemblyInfo.cs
+++ Xpdm.Math/AssemblyInfo.cs
@@ -23,6 +23,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -49,3 +50,5 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
+
+[assembly: CLSCompliant(true)]
